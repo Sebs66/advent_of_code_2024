66d074436288845eac0f963c979c14f45fc04cb2
sebs <sebamgc@gmail.com>
2024-12-13 22:55:05 +0100

    retrieve obstacles candidates

--- day_6/src/utils.rs
+++ day_6/src/utils.rs
@@ -7,6 +7,10 @@ use std::{
     io::{BufReader, Cursor, Read},
 };
 
+type ObstacleCoords = (i128, i128);
+type Direction = char;
+type GuardCoords = (i128, i128);
+
 fn init_directions() -> HashMap<char, (i8, i8)> {
     let mut map = HashMap::new();
     map.insert('^', (-1, 0));
@@ -94,7 +98,14 @@ pub fn calculate_movements(
     );
     let mut direction = guard_initial_direction;
     let mut direction_value = directions.get(&direction).unwrap();
-    check_next_obstacles(&obstacles_positions, direction, position);
+    let mut candidate_obstacles: Vec<(i128, i128)> = vec![];
+    candidate_obstacles = check_next_obstacles(
+        &obstacles_positions,
+        direction,
+        position,
+        candidate_obstacles,
+    );
+    println!("Candidate obstacles: {:?}", candidate_obstacles);
 
     loop {
         let next_proposed_position = (
@@ -138,24 +149,48 @@ fn check_next_obstacles(
     obstacles_positions: &Vec<(usize, usize)>,
     next_direction: char,
     position: (i128, i128),
-) {
+    candidate_obstacles: Vec<GuardCoords>,
+) -> Vec<GuardCoords> {
     println!(
         "Starting at: {:?}, we check for obstacles in the {} direction",
         position, next_direction
     );
-    let (mut is_an_obstacle, _, mut new_direction, mut new_position) =
-        find_next_obstacles(obstacles_positions, position, next_direction);
+    let (mut is_an_obstacle, _, mut new_direction, mut new_position, mut candidate_obstacles) =
+        find_next_obstacles(
+            obstacles_positions,
+            position,
+            next_direction,
+            candidate_obstacles,
+        );
     while is_an_obstacle {
-        (is_an_obstacle, _, new_direction, new_position) =
-            find_next_obstacles(obstacles_positions, new_position, new_direction);
+        (
+            is_an_obstacle,
+            _,
+            new_direction,
+            new_position,
+            candidate_obstacles,
+        ) = find_next_obstacles(
+            obstacles_positions,
+            new_position,
+            new_direction,
+            candidate_obstacles,
+        );
     }
+    return candidate_obstacles;
 }
 
 fn find_next_obstacles(
     obstacles_positions: &Vec<(usize, usize)>,
-    position: (i128, i128),
-    direction: char,
-) -> (bool, (i128, i128), char, (i128, i128)) {
+    position: GuardCoords,
+    direction: Direction,
+    mut candidate_obstacles: Vec<ObstacleCoords>,
+) -> (
+    bool,
+    ObstacleCoords,
+    Direction,
+    GuardCoords,
+    Vec<GuardCoords>,
+) {
     // println!(
     //     "find_next_obstacle: Looking for obstacles starting at {:?} in direction {}",
     //     position, direction
@@ -168,18 +203,29 @@ fn find_next_obstacles(
     // Position (row:1, col:9) and direction (row:1, col: 0)
     // Look for obstacles with row>1 and col=9
 
-    evaluate_candidate_position_for_obstacle(position, direction, obstacles_positions);
+    candidate_obstacles = evaluate_candidate_position_for_obstacle(
+        position,
+        direction,
+        obstacles_positions,
+        candidate_obstacles,
+    );
     let (is_obstacle, obstacle_coords, next_direction, new_position) =
         evaluate_next_obstacle(position, direction, obstacles_positions);
 
-    return (is_obstacle, obstacle_coords, next_direction, new_position);
+    return (
+        is_obstacle,
+        obstacle_coords,
+        next_direction,
+        new_position,
+        candidate_obstacles,
+    );
 }
 
 fn evaluate_next_obstacle(
     position: (i128, i128),
     direction: char,
     obstacles_positions: &Vec<(usize, usize)>,
-) -> (bool, (i128, i128), char, (i128, i128)) {
+) -> (bool, ObstacleCoords, Direction, GuardCoords) {
     let directions = init_directions();
     let direction_values = directions.get(&direction).unwrap();
     let coordinate_to_match = if direction_values.0 == 0 { 0 } else { 1 };
@@ -232,10 +278,45 @@ fn evaluate_next_obstacle(
 }
 
 fn evaluate_candidate_position_for_obstacle(
-    position: (i128, i128),
+    initial_guard_position: (i128, i128),
     direction: char,
     obstacles_positions: &Vec<(usize, usize)>,
-) {
+    mut candidate_obstacles: Vec<ObstacleCoords>,
+) -> Vec<ObstacleCoords> {
     let directions_mapper = init_directions();
-    let next_direction_mapper = init_next_direction();
+
+    let (_, obstacle_coords, new_direction, _) =
+        evaluate_next_obstacle(initial_guard_position, direction, obstacles_positions);
+
+    if obstacle_coords.0 == -1 || obstacle_coords.1 == -1 {
+        return candidate_obstacles;
+    }
+    println!(
+        "evaluate_candidate_position_for_obstacle: searching between {:?} and {:?} in {} direction",
+        initial_guard_position, obstacle_coords, direction
+    );
+    let direction_increment = *directions_mapper.get(&direction).unwrap();
+    let direction_increment = (direction_increment.0 as i128, direction_increment.1 as i128);
+
+    let mut candidate_coords = initial_guard_position;
+    while candidate_coords != obstacle_coords {
+        candidate_coords.0 += direction_increment.0;
+        candidate_coords.1 += direction_increment.1;
+        let (is_obstacle, obstacle_2_coords, _, _) =
+            evaluate_next_obstacle(candidate_coords, new_direction, obstacles_positions);
+
+        if is_obstacle {
+            candidate_coords.0 += direction_increment.0;
+            candidate_coords.1 += direction_increment.1;
+            if !(candidate_coords.0 == obstacle_coords.0 && candidate_coords.1 == obstacle_coords.1)
+            {
+                println!(
+                    "With a candidate obstacle at {:?}, we encounter an obtacle at {:?}!",
+                    candidate_coords, obstacle_2_coords
+                );
+                candidate_obstacles.push(candidate_coords);
+            }
+        }
+    }
+    return candidate_obstacles;
 }
